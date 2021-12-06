6724a56df319b8b54d4749ecf849415fb2f9a8bc
mango <m4ngo.root@protonmail.com>
2021-12-06 02:40:39 +0000

    :)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import checkers
 
 og_board = ["1","2","3","4","5","6","7","8","9"]
 board = [
@@ -36,7 +37,7 @@ class AiPlayer():
         other_player = 'o' if symbol == 'x' else 'x'
 
         #first we check if we are in a win situation
-        check_win()
+        checkers.check_win()
 
 """------------------------------º---------------------------º--------------------------------------"""
 
@@ -73,62 +74,6 @@ def insert(board):
         pass # as the turn is still the same there is no need for anything else 
 
 
-def draw_board(board):
-    print()
-    print("   ",board[0],"|", board[1],"|", board[2])
-    print("   ---|---|---")
-    print("   ",board[3],"|", board[4],"|", board[5])
-    print("   ---|---|---")
-    print("   ",board[6],"|", board[7],"|", board[8])
-    print()
-
-def check_win(board, turn):
-    if check_row(board) or check_columns(board) or check_diagonal(board):
-        if turn==1:
-            print("the winner is o")
-        else:
-            print("the winner is x")
-        return True
-    
-  
-def check_row(board):
-    row_1 = board[0] == board[1] == board[2] 
-    row_2 = board[3] == board[4] == board[5]
-    row_3 = board[6] == board[7] == board[8]
-    if row_1 or row_2 or row_3:
-        return True
-    
-    
-def check_columns(board):
-    column_1 = board[0] == board[3] == board[6]
-    column_2 = board[1] == board[4] == board[7]
-    column_3 = board[2] == board[5] == board[8]
-    if column_1 or column_2 or column_3:
-        return True
-    
-
-def check_diagonal(board):
-    diagonal_1 = board[0] == board[4] == board[8]
-    diagonal_2 = board[2] == board[4] == board[6]
-    if diagonal_1 or diagonal_2:
-        return True
-
-   
-def check_tie(board):
-    if not ("1" in board or "2" in board or "3" in board or "4" in board or "5" in board or "6" in board or "7" in board or "8" in board or "9" in board):
-        print("\n------TIE-----")
-        return True
-
-  
-def empty_squares(board):
-    return ' ' in board
-
-    
-def num_empty_squares(board):
-    return board.count(' ')
-
-def available_moves(board):
-    return [i for i, x in enumerate(board) if x==" "]
 
 def play(x_player, y_player):
     global turn
@@ -136,12 +81,12 @@ def play(x_player, y_player):
 
     play = True
     while play:
-        draw_board(board)
+        checkers.draw_board(board)
         insert(board)
         print("turn is now"+turn)
-        end = check_win(board, turn) or check_tie(board)
+        end = checkers.check_win(board, turn) or checkers.check_tie(board)
         if end:
-            draw_board(board)
+            checkers.draw_board(board)
             # play = False
             play_again = input("Play again? [y/n]").lower()
 
