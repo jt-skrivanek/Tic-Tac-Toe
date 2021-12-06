5fba2a83a8ab7f4bcdcfae57c4726dca01dd1458
JT Skrivanek <jtskrivanek@outlook.com>
2021-12-05 20:30:13 -0600

    reverting to bf64a205ffc92e55789b08c234166d69aaa502c9

--- main.py
+++ main.py
@@ -94,20 +94,8 @@ def play(game_instance):
         tictac.insert()
         end = tictac.check_win() or tictac.check_tie()
         if end:
-            print("Play again? [y/n]")
-            again = input()
-            # checks to see if y or n is inserted and stops the program or resets board based on outcome
-            if again == "n":
-                tictac.draw_board()
-                play = False
-            if again == "y":
-                tictac.board =[
-                "1","2","3",
-                "4","5","6",
-                "7","8","9"
-                    ]           
-                tictac.insert
-                play = True
+            tictac.draw_board()
+            play = False
     print("game ended mfs")
 
 if __name__== "__main__":
