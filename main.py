c0cadc45dc313f7da4737cd50d49c5b2b630075e
JT Skrivanek <jtskrivanek@outlook.com>
2021-12-13 22:59:45 -0600

    implemented AI although play again breaks after more than 1 use

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ moves = [] # keeps track of moves made
 x_player = HumanPlayer("O")
 y_player = HumanPlayer("X")
 
+print("[1]AI\n"+"[2]Human")
+choice = input()
+if choice == "1":
+    AI = True
+else:
+    AI = False
+
 def insert(board):
     global turn
     try:
@@ -41,28 +48,72 @@ def insert(board):
         print("invalid value try again")
         pass # as the turn is still the same there is no need for anything else 
 
+def insertAI(board):
+    global turn
+    try:
+        if turn == "x":
+            # self.x_player.make_move()
+            move = x_player.make_move()
+            # checks to see if y player already has 0 here
+            if board[move] == y_player.symbol or board[move] == x_player.symbol:
+                print("INVALID PLACE!")
+            else:
+                board[move] = x_player.symbol
+                turn = "y"
+                moves.append(move)
+
+        else:
+            # y_player.make_move()
+            move = y_player.moveAI()
+            # checks to see if x player already has X here
+            if board[move] == x_player.symbol or board[move] == y_player.symbol:
+                print("INVALID PLACE!")
+            else:
+                board[move] = y_player.symbol
+                turn = "x"
+                moves.append(move)
+    except IndexError or ValueError:
+        print("invalid value try again")
+        pass # as the turn is still the same there is no need for anything else
+
 def play(x_player, y_player):
     global turn
     global board
-
-    play = True
-    while play:
-        checkers.draw_board(board)
-        insert(board)
-        end = checkers.check_win(board, turn) or checkers.check_tie(board)
-        if end:
+    global AI
+    
+    if AI == False:
+        while 1 == 1:
             checkers.draw_board(board)
-            # play = False
-            play_again = input("Play again? [y/n]").lower()
+            insert(board)
+            end = checkers.check_win(board, turn) or checkers.check_tie(board)
+            if end:
+                checkers.draw_board(board)
+                play_again = input("Play again? [y/n]").lower()
 
-            if play_again == "y":
-                board = og_board
+                if play_again == "y":
+                    board = og_board
 
-            else:
-                print("game ending....")
-                play = False
-    print("Game Over!")
+                else:
+                    print("game ending....")
+                    print("Game Over!")
+                    exit()
+
+    if AI == True:
+        while 1 == 1:
+            checkers.draw_board(board)
+            insertAI(board)
+            end = checkers.check_win(board, turn) or checkers.check_tie(board)
+            if end:
+                checkers.draw_board(board)
+                play_again = input("Play again? [y/n]").lower()
+
+                if play_again == "y":
+                    board = og_board
+
+                else:
+                    print("game ending....")
+                    print("Game Over!")
+                    exit()
 
 if __name__== "__main__":
-
     play(x_player, y_player)
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import checkers
+import random
 # player class
 class HumanPlayer():
     def __init__(self, symbol):
@@ -6,4 +7,8 @@ class HumanPlayer():
 
     def make_move(self):
         move = int(input(f"choose a position {self.symbol} player: ")) - 1
+        return move
+
+    def moveAI(self):
+        move = random.randint(0,8)
         return move
